386e2c57b5a1c912f2dd8b6d7e73bd5e2a12d7d9
Qiu Xiaobing <qiu.xiaobing@ext.deposit-solutions.com>
2019-12-06 15:59:44 +0800

    修改项目名称

--- src/main/java/cn/withive/wxpay/WxpayApplication.java
+++ src/main/java/cn/withive/wxpay/WxpayApplication.java
@@ -2,10 +2,18 @@ package cn.withive.wxpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class WxpayApplication {
+public class WxpayApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(WxpayApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WxpayApplication.class, args);
